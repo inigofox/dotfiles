a4c885213f43ea389bcb4a279e21995bd8aa4ad5
Daniel Rodriguez <df.rodriguez143@gmail.com>
2015-12-18 12:53:21 -0600

    Update

--- .oh-my-zsh/custom/plugins/docker2/docker2.plugin.zsh
+++ .oh-my-zsh/custom/plugins/docker2/docker2.plugin.zsh
@@ -2,13 +2,13 @@ local test_=$(docker-machine status default) 2>/dev/null
 if [[ $test_ == "Running" ]]; then
     eval "$(docker-machine env default)"
 
-    alias docker-cmd="docker run -t -i"
+    alias docker-cmd="docker run -it"
     docker-build() { docker build -t=\'$1\' . }
-    docker-bash() { docker run -t -i $1 /bin/bash }
+    docker-bash() { docker run -it $1 /bin/bash }
     docker-stop-all() { docker stop $(docker ps -a -q) }
     docker-rm-stop() { docker rm $(docker ps -a -q) }
     docker-rmi-none() { docker rmi $(docker images | grep "^<none>" | awk '{print $3}') }
-    docker-clean() { docker-stop-all docker-rm-stop docker-rmi-none }
+    docker-clean() { docker-stop-all; docker-rm-stop; docker-rmi-none; }
     # docker-salt() { docker run -t -i -v $2:/srv/salt -v $3:/srv/pillar $1 /bin/bash }
 else
     # echo "docker-machine not running"
